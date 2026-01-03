55432b01e3a65f53e714385d8c9eab5127003774
Mark Fox <mf@ixel.org>
2026-01-02 22:41:02 -0800

    Make things prettier

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,4 @@
-import packageJson from './package.json' with { type: 'json' }
+import packageJson from './package.json' with { type: 'json' };
 
 export default [
   {
@@ -14,4 +14,4 @@ export default [
       },
     ],
   },
-]
+];
